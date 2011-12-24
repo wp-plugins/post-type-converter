e6c530925378a0342bdaaefe62ac880e9ddd000e
kevinlangleyjr <kevinlangleyjr@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-12-24 13:25:15 +0000

    Fixing version number and Copyright GPL notice within main plugin file
    git-svn-id: https://plugins.svn.wordpress.org/post-type-converter/trunk@480201 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- post-type-converter.php
+++ post-type-converter.php
@@ -4,8 +4,26 @@ Plugin Name: Post Type Converter
 Plugin URI: http://voceconnect.com
 Description: Gives a qualified user the ability to convert a post from one post_type to another public post_type.
 Author: Kevin Langley
-Version: 1.0
+Version: 0.1
 Author URI: http://voceconnect.com
+*******************************************************************
+Copyright 2011-2012 Kevin Langley (email : klangley@voceconnect.com)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*******************************************************************
+*/
+
 */
 if(!class_exists('Post_Type_Converter')) {
 
