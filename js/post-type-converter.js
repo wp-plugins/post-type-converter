a5804ae18196f883a615f5a802956a5116c96739
kevinlangleyjr <kevinlangleyjr@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-02-17 12:35:00 +0000

    Adding latest js
    git-svn-id: https://plugins.svn.wordpress.org/post-type-converter/trunk@506461 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/post-type-converter.js
+++ js/post-type-converter.js
@@ -1,4 +1,4 @@
-jQuery(function($){
+jQuery(function(){
 	
 	var options = '<option value="-1">Convert Post Type</option>';
 	for(var index in script_vars){
@@ -7,6 +7,6 @@ jQuery(function($){
 	var top_html = '<div class="alignleft actions"><select name="change_post_type">'+options+'</select></div><input type="submit" name="" id="apply_convert_post_type" class="button-secondary action" value="Convert">';
 	var bottom_html = '<div class="alignleft actions"><select name="change_post_type2">'+options+'</select></div><input type="submit" name="" id="apply_convert_post_type" class="button-secondary action" value="Convert">';
 	
-	$(top_html).insertBefore('.tablenav.top .tablenav-pages');
-	$(bottom_html).insertBefore('.tablenav.bottom .tablenav-pages');
+	jQuery(top_html).insertBefore('.tablenav.top .tablenav-pages');
+	jQuery(bottom_html).insertBefore('.tablenav.bottom .tablenav-pages');
 });
